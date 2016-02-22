69f9e1d5da752c4bd922a7a3d83ef2ead4727dc6
Diogo Carolo <diogocarolo@yahoo.com.br>
2016-02-22 18:46:34 -0300

    remoção dos arquivos da pasta de src

--- admin-src/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.botao-menu').click(function() {
-        $('.menu').animate({
-            left: parseInt($('.menu').css('left'),10) == 0 ? -$('.menu').outerWidth() : 0
-        });
-    });
-}
--- admin-src/node_modules/kouto-swiss/_docs/usage/gulp.md
+++
@@ -1,3 +0,0 @@
-# Included in compilation with gulp
-
-To use **kouto swiss** with gulp, use [gulp-stylus](https://www.npmjs.org/package/gulp-stylus) and include **kouto swiss** in your `use` option for the task.
