1f9c65635f8b7030d8fb9c66b2b763e07a5b3f97
Diego T. Fialho <diego@maxisite.net>
2016-04-04 12:51:25 -0300

    Multiple user columns in AuthHelper

--- system/helpers/AuthHelper.php
+++ system/helpers/AuthHelper.php
@@ -11,6 +11,7 @@ class AuthHelper
 	private $_tableName;
 	private $_userColumn		= 'login';
 	private $_senhaColumn		= 'senha';
+	private $_userNameColumn;
 	private $_user;
 	private $_senha;
 	private $_controllerLogado 	= 'admin';
@@ -55,8 +56,24 @@ class AuthHelper
 		//PASSO A TABELA QUE ESTÁ OS DADOS PARA LOGIN
 		$this -> _bd->_tabela = $this->_tableName;
 
+		//VERIFICA SE É MAIS DE UMA COLUNA PARA O LOGIN E MONTA A STRING
+		if(is_array($this->_userColumn))
+		{
+			$strUser = "(";
+			foreach ($this->_userColumn as $key => $str) {
+				$strUser .= $str . " = '" . $this ->_user . "' ";
+				
+				if($key != count($this->_userColumn) - 1)
+					$strUser .= " OR ";
+				else
+					$strUser .= ")";
+			}
+		}
+		else
+			$strUser = $this->_userColumn . " = '" . $this->_user . "'";
+
 		//DEFINO A STRING DE COMPARAÇÃO
-		$where = $this->_userColumn . " = '" . $this->_user . "' AND " . $this->_senhaColumn . " = '" . hash('sha512', $this->_senha) . "'";
+		$where = $strUser . " AND " . $this->_senhaColumn . " = '" . hash('sha512', $this->_senha) . "'";
 
 		//FAZ A CONSULTA
 		$sql = $this -> _bd -> readLine($where);
@@ -64,7 +81,20 @@ class AuthHelper
 		//SE EXISTIR ELE LOGA E CRIA A SESSÃO
 		if(count($sql) > 0 AND !empty($sql) )
 		{
-			$_SESSION['user'] = $sql[$this->_userColumn];
+			//VERIFICA SE SÃO VÁRIAS COLUNAS
+			if(is_array($this->_userColumn))
+			{
+				//VERIFICA SE A COLUNA DO NOME DO USUÁRIO ESTÁ SETADA
+				if(!empty($this->_userNameColumn))
+					$_SESSION['user'] = $sql[$this->_userNameColumn];
+				
+				//SE NÂO ESTIVER PEGA A PRIMEIRA POSIÇÃO DO ARRAY
+				else
+					$_SESSION['user'] = $sql[$this->_userColumn[0]];
+			}
+			else
+				$_SESSION['user'] = $sql[$this->_userColumn];
+
 			$_SESSION['dados_usuario'] = $sql;
 			$_SESSION['logado'] = 1;
 			$_SESSION['hash'] = sha1($this->database['banco']);
